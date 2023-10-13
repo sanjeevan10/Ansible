684686f2e560a0f0963bf6f18a998fb0c3106e2e
Sanjeevan Sivasubramaniam <sanjeevan.siva@gmail.com>
2023-10-13 21:18:42 +0100

    .

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-#from flask_sqldb import MySQL      # For newer versions of flask-mysql 
+from flask_sqldb import MySQL      # For newer versions of flask-mysql 
 # from flask.ext.mysql import MySQL   # For older versions of flask-mysql
 app = Flask(__name__)
 
